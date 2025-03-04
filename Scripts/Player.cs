cfd75e6900798b7bfe681eb27aef3595c5f3a97e
Abubaker Tariq <abubakertariq1001@gmail.com>
2025-03-05 02:33:54 +0500

    Collision logic

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [Space] [Header("Configurable values")]
-    [SerializeField] private float moveSpeed = 5f;
+    [Space] [Header("Player settings")]
+    [SerializeField] private float playerRadius = 0.7f;
+    [SerializeField] private float playerHeight = 1.5f;
+    [SerializeField] private float moveSpeed = 7.5f;
     [SerializeField] private float rotateSpeed = 5f;
 
     [Space] [Header("Component references")]
@@ -15,11 +17,50 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        // Getting input here
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new(inputVector.x, 0f, inputVector.y);
-        transform.position += moveSpeed * Time.deltaTime * moveDir;
-        transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
+        
+        // Movement logic
+        float moveDistance = moveSpeed * Time.deltaTime;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
 
+        if (!canMove)
+        {
+            // Attempt only X Axis movement
+            Vector3 moveDirX = new Vector3(moveDir.x, 0f, 0f).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+
+            if (canMove)
+            {
+                moveDir = moveDirX;
+            }
+            else
+            {
+                // Attempt only Z Axis movement
+                Vector3 moveDirZ = new Vector3(0f, 0f, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+
+                if (canMove)
+                {
+                    moveDir = moveDirZ;
+                }
+                else
+                {
+                    // Can't move in any direction
+                }
+            }
+        }
+        
+        if (canMove) 
+        {
+            transform.position += moveDistance * moveDir;
+        }
+
+        // Smooth rotation
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
+        
+        // Move animation
         playerAnimator.SetBool(IsWalking, moveDir != Vector3.zero);
     }
 }
